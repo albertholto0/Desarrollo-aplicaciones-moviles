c3d3d37dd66116ea93b44b69f4ee890183f0f81d
Albert Contreras <albertmendoza019@gmail.com>
2025-06-04 12:08:43 -0600

    Pequeños cambios en el tema

--- uni_places/lib/configurations/app_theme.dart
+++ uni_places/lib/configurations/app_theme.dart
@@ -3,8 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 
 final appColorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
-  seedColor: const Color.fromARGB(255, 35, 125, 31),
-  surface: const Color.fromARGB(255, 125, 226, 120),
+  seedColor: const Color.fromARGB(255, 56, 142, 60),
+  surface: const Color.fromARGB(255, 35, 45, 35),
+  primary: const Color.fromARGB(255, 67, 160, 71),
+  secondary: const Color.fromARGB(255, 102, 187, 106),
 );
 
 final appTextTheme = GoogleFonts.nunitoTextTheme().copyWith(
@@ -13,4 +15,10 @@ final appTextTheme = GoogleFonts.nunitoTextTheme().copyWith(
   titleSmall: GoogleFonts.nunito(fontWeight: FontWeight.bold, fontSize: 18.0),
   titleMedium: GoogleFonts.nunito(fontWeight: FontWeight.bold, fontSize: 22.0),
   titleLarge: GoogleFonts.nunito(fontWeight: FontWeight.bold, fontSize: 28.0),
+);
+
+final appTheme = ThemeData().copyWith(
+  scaffoldBackgroundColor: appColorScheme.surface,
+  colorScheme: appColorScheme,
+  textTheme: appTextTheme,
 );
